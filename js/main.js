26b388573c1af1243b291032a7395d03b343fef2
Jolanta Huba <huba.jolanta@gmail.com>
2022-07-05 11:54:02 +0200

    Add active section switching code

--- js/main.js
+++ js/main.js
@@ -2,15 +2,42 @@
 
 const createBtn = document.querySelector('.menu__create-btn');
 const findBtn = document.querySelector('.menu__find-btn');
-const container = document.querySelector('.container');
+const menuBtns = document.querySelectorAll('.menu__btn');
+
+const menuToggler = document.querySelector('.menu__toggler');
+const sections = [...document.querySelectorAll('.section')];
 const inputFile = document.getElementById('input-file');
 const exportBtn = document.getElementById('export-btn');
 
 createBtn.addEventListener('click', () => {
-  container.classList.remove('find-view')
+  sections
+  .find(section => section.classList.contains('active'))
+  .classList.remove('active')
+
+  sections
+  .find(section => section.id == 'create')
+  .classList.add('active');
+
+  findBtn.classList.remove('active');
+  createBtn.classList.add('active');
+
+  menuToggler.classList.remove('switched');
+
 });
+
 findBtn.addEventListener('click', () => {
-  container.classList.add('find-view')
+  sections
+  .find(section => section.classList.contains('active'))
+  .classList.remove('active')
+
+  sections
+  .find(section => section.id == 'find')
+  .classList.add('active');
+
+  createBtn.classList.remove('active');
+  findBtn.classList.add('active');
+  
+  menuToggler.classList.add('switched');
 });
 
 inputFile.addEventListener('change', (e) => {
